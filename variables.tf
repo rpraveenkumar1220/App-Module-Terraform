f8c2548d27653df14060ee5a5cb7f026a169277e
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-01-14 19:46:14 +0530

    Parameters adjustment

--- variables.tf
+++ variables.tf
@@ -12,5 +12,7 @@ variable "sg_subnet_cidr" {}
 variable "app_port" {}
 variable "listener_arn" {}
 variable "lb_rule_priority" {}
+variable "kms_key_arn" {}
+variable "extra_param_access" {}
 /*variable "subnet_id" {}*/
 
